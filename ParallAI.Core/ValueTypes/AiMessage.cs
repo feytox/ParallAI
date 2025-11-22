de7ce41e0c062744e7594b8f9481aef0c11c8c01
ivolzok <ivolzok@gmail.com>
2025-11-23 02:19:14 +0500

    Add role to AiMessage

--- ParallAI.Core/ValueTypes/AiMessage.cs
+++ ParallAI.Core/ValueTypes/AiMessage.cs
@@ -1,15 +1,24 @@
 ﻿namespace ParallAI.Core.ValueTypes;
 
-public abstract record AiMessage;
+public abstract record AiMessage(Role role = Role.User)
+{
+    public Role Role { get; init; } = role;
+}
 
-public record TextMessage(string Text) : AiMessage;
+public record TextMessage(string Text, Role role = Role.User) : AiMessage(role);
 
-public record FileMessage(string Text, AiFileInfo[] Files) : TextMessage(Text)
+public record FileMessage(string Text, AiFileInfo[] Files, Role role) : TextMessage(Text, role)
 {
     private const string DefaultText = "Describe content";
 
-    public static FileMessage Create(AiFileInfo[] files, string? text)
+    public static FileMessage Create(AiFileInfo[] files, string? text, Role role = Role.User)
     {
-        return new FileMessage(text ?? DefaultText, files);
+        return new FileMessage(text ?? DefaultText, files, role);
     }
+}
+
+public enum Role
+{
+    User,
+    Assistant,
 }
