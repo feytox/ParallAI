ffd5111e33b7dc6a2477218471ae3f11ef47e962
Feytox <73577753+feytox@users.noreply.github.com>
2025-12-06 17:37:24 +0500

    feat: models/presets count validation

--- ParallAI.TeleBot/Commands/CompareCommand.cs
+++ ParallAI.TeleBot/Commands/CompareCommand.cs
@@ -2,6 +2,7 @@ using ParallAI.Core.Repositories;
 using ParallAI.TeleBot.Callback;
 using ParallAI.TeleBot.Commands.UI;
 using ParallAI.TeleBot.Core.Commands.Common;
+using ParallAI.TeleBot.Util;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
@@ -15,6 +16,12 @@ public class CompareCommand(IRepository<User, long> users) : UserCommand(users)
 {
     protected override async Task Execute(Message message, ITelegramBotClient bot, User user)
     {
+        if (!await ValidationHelper.ValidateModelsCount(message, bot, user) 
+            || !await ValidationHelper.ValidatePresetsCount(message, bot, user))
+        {
+            return;
+        }
+
         var buttons = user.ComparisonDates
             .Select(time => time.ToUniversalTime().ToString("yyyy/MM/dd HH:mm z"))
             .Select((text, i) => CompareCallback.CreateButton(text, $"{i}"))
